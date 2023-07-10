6fcb77e22d67be96abeaa1e94beaba4f93002721
Douglas Lowder <douglowder@mac.com>
2023-07-10 01:43:33 -0700

    Force splash screen to never hide

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   console.log("oigoufiiuvi");
